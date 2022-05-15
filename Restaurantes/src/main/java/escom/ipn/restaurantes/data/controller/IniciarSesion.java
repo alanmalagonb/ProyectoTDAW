f16c20b8a15b863d860db7eba7fbbbd908148a05
Alan Malagon <alanmalagonb@gmail.com>
2022-05-15 16:45:16 -0500

    Servlets Trabajador

---
+++ Restaurantes/src/main/java/escom/ipn/restaurantes/data/controller/IniciarSesion.java
@@ -0,0 +1,94 @@
+package escom.ipn.restaurantes.data.controller;
+
+import escom.ipn.restaurantes.data.dao.TrabajadorDAO;
+import escom.ipn.restaurantes.data.dto.TrabajadorDTO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author alanm
+ */
+@WebServlet(name = "IniciarSesion", urlPatterns = {"/IniciarSesion"})
+public class IniciarSesion extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            String nombre = request.getParameter("nombre");
+            String apellidoMaterno = request.getParameter("materno");
+            String apellidoPaterno = request.getParameter("paterno");
+            String email = request.getParameter("email");
+            String password = request.getParameter("password");
+            TrabajadorDTO dto = new TrabajadorDTO();
+            dto.getTrabajador().setNombreTrabajador(nombre);
+            dto.getTrabajador().setApellidoMaterno(apellidoMaterno);
+            dto.getTrabajador().setApellidoPaterno(apellidoPaterno);
+            dto.getTrabajador().setEmail(email);
+            dto.getTrabajador().setPassword(password);
+            try{
+                TrabajadorDAO dao = new TrabajadorDAO();
+                dto = dao.login(dto);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            /* SESIÓN */
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
